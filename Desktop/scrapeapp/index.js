03005b17ef8e3845e50ac7cf18bcf75d2e5f8dfa
谢小东 <xiexiaodong@126.com>
2020-10-29 14:49:03 +0800

    sdaf

--- Desktop/scrapeapp/index.js
+++ Desktop/scrapeapp/index.js
@@ -12,7 +12,8 @@ async function bootstarp() {
 
 	  console.log('启动爬取数据服务'.info);
 	  const browser = await puppeteer.launch({
-	  	timeout: 30000
+	  	timeout: 30000,
+	  	headless: false
 	  })
 	  console.log('正在启动浏览器'.info);
 
@@ -20,15 +21,37 @@ async function bootstarp() {
 
 	  	const page = await browser.newPage()
 	    page.on('console', msg => {
+	      console.log('监听浏览器控制台的日志输出可忽略'.info);
 	      if (typeof msg === 'object') {
-	        console.log(msg.error)
+
+	        console.log(JSON.stringify(msg).error)
 	      } else {
-	        console.log(msg.error);
+	        console.log(msg);
 	      }
 	    })
-	    await page.goto('http://zcdx.bda.gov.cn/#/themeTypeDetail?themeTypeId=770294634592575488');
+
+	    await page.goto('http://zcdx.bda.gov.cn');
+
+	    const aHandle = await page.evaluateHandle(() => document.body);
+
+		const resultHandle = await page.evaluateHandle(body => body.innerHTML, aHandle);
+		console.log(await resultHandle.jsonValue());
+		await resultHandle.dispose();
+
+
+		// page.on('response', async response => {
+		// 	console.log('get api data:::',response.url());
+		// 	if(response.url() =='http://zcdx.bda.gov.cn/lron/v2/bones/th/policy/search?region=110115403&pageSize=8&enabled=Y&sort=11&remote=Y'){
+		// 		 const data=await response.json();
+		// 		 console.log(data);
+		// 	}
+		   
+		// })
+	    
+
+
 	  }catch(e){
-	  	 console.log(e);
+	  	console.log(e);
 	  	await browser.close()
 	  }finally{
 	  	//process.exit(0)
